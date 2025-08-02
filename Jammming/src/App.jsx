d19b4c53d4338bffe8bf071f839c12d2a4d78cf2
Your Name <polem@Poleming.>
2025-08-02 18:14:23 -0400

    Added authentication and deployment to github pages

--- Jammming/src/App.jsx
+++ Jammming/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "https://jose69420xxx.github.io/Jamming/index.html");
+    params.append("redirect_uri", "https://jose69420xxx.github.io");
     params.append("scope", "user-read-private user-read-email playlist-modify-private playlist-modify-public");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -46,7 +46,7 @@ function App() {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "https://jose69420xxx.github.io/Jamming/index.html");
+    params.append("redirect_uri", "https://jose69420xxx.github.io");
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
